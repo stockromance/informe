ec300da19d5b27548d9aa022e1082c965d7b1890
stockromance <94761827+stockromance@users.noreply.github.com>
2022-02-19 09:36:20 -0300

    Add files via upload

--- import.js
+++ import.js
@@ -1,6 +1,7 @@
 var input = document.getElementById('input');
 var list = document.getElementById('list'); 
 
+
 input.addEventListener('change', function() 
     {
         readXlsxFile(input.files[0]).then(function(data) 
@@ -16,10 +17,49 @@ input.addEventListener('change', function()
                 
                 list.innerHTML += "<br/>{id:'"+data[row][0]+"', nombre:'"+data[row][1]+"'}";
                 list.innerHTML += "<br/>];";
-
-                data.forEach(([key, value]) => {
-                console.log("{id:'"+key+"', nombre:'"+value+"'},");});
             }
         );
+    }
+);
+
+
+// EXPORTAR JS
+
+var botonJS = document.getElementById('botonJS'); 
+
+function exportar (data, fileName) 
+{
+    const a = document.createElement("a");
+    const contenido = data,
+        blob = new Blob([contenido], {type: "octet/stream"}),
+        url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = fileName;
+    a.click();
+    window.URL.revokeObjectURL(url);    
+};
+
+botonJS.addEventListener('click', function() 
+    {
+        const data = document.querySelector('#list').textContent;
+        const nombreArchivo = 'items.js'
+        exportar(data, nombreArchivo);
+    }
+);
+
+// EXPORTAR XLSX
+
+var botonXLSX = document.getElementById('botonXLSX'); 
+
+function fnExportToExcel (fileExtension, fileName)
+{
+       var elt = document.getElementById('tabla');
+       var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
+       return XLSX.writeFile(wb, fileName+"."+fileExtension || ('MySheetName.' + (fileExtension || 'xlsx')));
+};
+
+botonXLSX.addEventListener('click', function()     
+    {       
+        fnExportToExcel('xlsx', 'factura');
     }
 );
